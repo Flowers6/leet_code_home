4d247c9276cd159883df00137cd312817deeaccc
flowers6 <3216609595@qq.com>
2026-07-02 20:09:45 +0800

    feat(medium): 实现字符串中单词反转功能
    - 添加 Java util 包导入用于数组和集合操作
    - 使用正则表达式去除多余空格和首尾空格
    - 通过空格分割字符串为单词列表
    - 利用 Collections.reverse 方法反转单词顺序
    - 最后将反转后的单词重新组合成字符串返回

--- train_java/src/main/java/medium/ReverseWordsInAString.java
+++ train_java/src/main/java/medium/ReverseWordsInAString.java
@@ -1,5 +1,9 @@
 package medium;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author : Flowers6
  * @version : v1.0
@@ -10,7 +14,22 @@ package medium;
 public class ReverseWordsInAString {
 
     public String reverseWords(String s) {
-        return null;
+
+        // 去除多余空格正则
+        String distinctSpace = "\s+";
+
+        // 去除首尾空格正则
+        String trimSpace = "^\s+|\s+$";
+        s = s.replaceAll(distinctSpace, " ");
+        s = s.replaceAll(trimSpace, "");
+
+        // 按空格分割字符串
+        List<String> strList = Arrays.asList(s.split("\s"));
+
+        // 反转
+        Collections.reverse(strList);
+
+        return String.join("\s", strList);
     }
 
 }
