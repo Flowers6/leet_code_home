c72ba9de04529558569751a6053776386c669836
flowers6 <3216609595@qq.com>
2026-07-07 18:20:23 +0800

    feat(medium): 添加随机链表复制功能
    - 实现了带有随机指针的链表复制算法
    - 使用三次遍历完成原地复制避免额外空间
    - 第一次遍历创建新节点并插入原链表
    - 第二次遍历设置随机指针指向对应的新节点
    - 第三次遍历分离原链表和复制链表
    - 定义了包含值、下一节点和随机指针的节点类

---
+++ train_java/src/main/java/medium/CopyListWithRandomPointer.java
@@ -0,0 +1,58 @@
+package medium;
+
+/**
+ * @author : Flowers6
+ * @version : v1.0
+ * @description :
+ * @date : 2026/7/7
+ * @time : 16:28
+ */
+public class CopyListWithRandomPointer {
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            Node copy = new Node(curr.val);
+            copy.next = next;
+            curr.next = copy;
+            curr = next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        Node res = head.next;
+        curr = head;
+        while (curr != null) {
+            Node copy = curr.next;
+            curr.next = copy.next;
+            if (copy.next != null) {
+                copy.next = copy.next.next;
+            }
+            curr = curr.next;
+        }
+
+        return res;
+    }
+
+    public static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+}
