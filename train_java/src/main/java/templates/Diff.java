10ddf817e90189b27b705c4d0babda8e59d4e2a0
flowers6 <3216609595@qq.com>
2026-07-11 18:13:27 +0800

    feat(algorithm): 添加差分数组模板类
    - 实现差分数组的初始化构造方法
    - 添加区间增量操作功能
    - 提供结果数组返回方法
    - 支持对指定区间的增减操作
    - 实现差分数组到原数组的转换逻辑

---
+++ train_java/src/main/java/templates/Diff.java
@@ -0,0 +1,42 @@
+package templates;
+
+/**
+ * @author : Flowers6
+ * @version : v1.0
+ * @description :
+ * @date : 2026/7/11
+ * @time : 18:06
+ */
+public class Diff {
+
+    // 差分数组
+    private int[] diff;
+
+    // 输入一个初始数组，区间操作将在这个数组上进行
+    public Diff(int[] nums) {
+        diff = new int[nums.length];
+        diff[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            diff[i] = nums[i] - nums[i - 1];
+        }
+    }
+
+    // 给闭区间 [i, j] 增加 val（可以是负数）
+    public void increment(int i, int j, int val) {
+        diff[i] += val;
+        if (j + 1 < diff.length) {
+            diff[j + 1] -= val;
+        }
+    }
+
+    // 返回结果数组
+    public int[] result() {
+        int[] res = new int[diff.length];
+        res[0] = diff[0];
+        for (int i = 1; i < diff.length; i++) {
+            res[i] = res[i - 1] + diff[i];
+        }
+        return res;
+    }
+
+}
