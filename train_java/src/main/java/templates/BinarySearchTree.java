bd880df25cf324aa7ee589011d9898ac082fa101
flowers6 <3216609595@qq.com>
2026-07-12 14:35:40 +0800

    feat(bst): 添加二叉搜索树层序遍历功能
    - 导入 LinkedList 和 Queue 数据结构支持
    - 实现 levelOrderTraverse 方法进行层序遍历
    - 使用队列存储待访问节点实现广度优先搜索
    - 添加空节点检查确保遍历安全
    - 实现节点值打印和子节点入队逻辑

--- train_java/src/main/java/templates/BinarySearchTree.java
+++ train_java/src/main/java/templates/BinarySearchTree.java
@@ -2,6 +2,9 @@ package templates;
 
 import common.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author : Flowers6
  * @version : v1.0
@@ -21,6 +24,27 @@ public class BinarySearchTree {
         // 中序位置
         traverse(root.right);
         // 后序位置
+    }
+
+    void levelOrderTraverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            // 访问 cur 节点
+            System.out.println(cur.val);
+
+            // 把 cur 的左右子节点加入队列
+            if (cur.left != null) {
+                q.offer(cur.left);
+            }
+            if (cur.right != null) {
+                q.offer(cur.right);
+            }
+        }
     }
 
 }
